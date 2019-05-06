7348cb62d58bce8db2a88863c20dddddfbe8a21c
YeChenTao <ye19980620@163.com>
2019-05-06 11:04:32 +0800

    动态规划

--- Algorithm/DynamicProgramming/EditDistance.cpp
+++ Algorithm/DynamicProgramming/EditDistance.cpp
@@ -9,16 +9,22 @@
 #include <algorithm>
 using namespace std;
 
-int editDistance(string s1, string s2){
+int editDistance(string s1, string s2)
+{
     int len1 = s1.length(), len2 = s2.length();
-    vector<vector<int>> dp(len1+1, vector<int>(len2+1, 0));
-    for(int i = 1;i <= len1;i++)
+    vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
+    for (int i = 1; i <= len1; i++)
         dp[i][0] = i;
-    for(int i = 1;i <= len2;i++)
+    for (int i = 1; i <= len2; i++)
         dp[0][i] = i;
-    for(int i = 1;i <= len1;i++){
-        for(int j = 1;j <= len2;j++){
-            dp[i][j] = min(dp[i-1][j-1]+(s1[i-1] == s2[j-1] ? 0 : 1), min(dp[i-1][j]+1, dp[i][j-1]+1));
+    for (int i = 1; i <= len1; i++)
+    {
+        for (int j = 1; j <= len2; j++)
+        {
+            dp[i][j] = min(dp[i - 1][j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1), min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+            //dp[i-1][j]+1表示删掉字符串s1最后一个字符s1[i]
+            //dp[i][j-1]+1表示给字符串添加s2最后一个字符
+            //dp[i-1][j-1]+?表示改变,相同则不需操作次数+0,不同则需要+1
         }
     }
     return dp[len1][len2];
